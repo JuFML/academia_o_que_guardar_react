5afcbd04b254b9f12c083b96a856f8244625318d
Juliana Fernandez <julianafernandezlima@hotmail.com>
2023-11-20 12:21:19 -0300

    Criado componente Stats

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,32 @@ const Filters = ({ orderBy, onChangeOrder }) => {
   );
 };
 
+const Stats = ({ listObjects }) => {
+  const singularPlural = listObjects.length === 1 ? "item" : "itens";
+  const storedItems = listObjects.reduce(
+    (acc, item) => (item.stored ? acc + 1 : acc),
+    0
+  );
+  const storedPercentage =
+    storedItems.length === 0
+      ? 0
+      : ((storedItems / listObjects.length) * 100).toFixed(0);
+
+  return (
+    <footer>
+      <p>
+        Você tem {listObjects.length} {singularPlural} na lista
+        {listObjects.length > 0 && (
+          <span>
+            {" "}
+            e já guardou {storedItems} ({storedPercentage}%)
+          </span>
+        )}
+      </p>
+    </footer>
+  );
+};
+
 const App = () => {
   const [listObjects, setListObjects] = useState(initialValue);
   const [orderBy, setOrderBy] = useState("recentes");
@@ -140,9 +166,8 @@ const App = () => {
 
         <Filters orderBy={orderBy} onChangeOrder={setOrderBy} />
       </main>
-      <footer>
-        <p>Você tem 0 itens na lista</p>
-      </footer>
+
+      <Stats listObjects={listObjects} />
     </>
   );
 };
