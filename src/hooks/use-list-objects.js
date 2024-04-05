3fb06b3826a345c7c117f05dfe23111feb2d2597
JuFML <julianafernandezlima@hotmail.com>
2024-04-05 11:11:17 -0300

    ✅ data persistence with localForage

--- src/hooks/use-list-objects.js
+++ src/hooks/use-list-objects.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import localforage from "localforage";
+import { useState, useEffect } from "react";
 
 const useListObjects = () => {
   const [listObjects, setListObjects] = useState([]);
   const [orderBy, setOrderBy] = useState("recentes");
+
+  useEffect(() => {
+    localforage.setItem("guardaCoisas", listObjects)
+      .catch((error) => alert(error.message))
+  }, [listObjects])
+
+  useEffect(() => {
+    localforage.getItem("guardaCoisas")
+      .then((value) => {
+        if (value) {
+          setListObjects(value)
+        }
+      })
+      .catch(error => alert(error.message))
+  }, [])
 
   const handleDelete = (id) => {
     const newListObject = listObjects.filter((obj) => obj.id !== id);
